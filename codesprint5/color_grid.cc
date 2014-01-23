79ec68ede61d4ee4f0caf27531643a7ffd380ef1
washingtonzju <zhongkai.mi@gmail.com>
2014-01-23 18:30:14 +0800

    Create color_grid.cc

---
+++ codesprint5/color_grid.cc
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int 
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int T, N, M, K;
+    scanf("%d", &T);
+    for(int i=0;i<T;++i)
+    {
+        scanf("%d%d%d", &N, &M, &K);
+    }
+    return 0;
+}
